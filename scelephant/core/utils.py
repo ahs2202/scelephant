c1971431bbed94b58daba9a226a36f672e35f486
merit_an <ahs2202@gm.gist.ac.kr>
2023-09-27 23:16:36 +0900

    regular update

--- scelephant/core/utils.py
+++ scelephant/core/utils.py
@@ -6595,11 +6595,13 @@ def MTX_Convert_10x_MEX_to_10x_HDF5_Format(
     path_folder_matrix_input_mex_format: str,
     path_file_matrix_output_hdf5_format: str,
     name_genome: str = "unknown",
+    flag_round_float : bool = True,
 ):
     """# 2023-09-15 01:36:49
     path_folder_matrix_input_mex_format : str # the path of the input 10x MEX matrix folder
     path_file_matrix_output_hdf5_format : str # the path of the output 10x HDF5 matrix file
     name_genome : str = 'unknown' # the name of the genome
+    flag_round_float : bool = True, # if True, if float values in the matrix is detected, round the values to convert to the integer values. However, when the value becomes 0 after being rounded, change the values back to 1 (to avoid exporting records with zero count values).
     """
     """ import libaries """
     import h5py
@@ -6646,6 +6648,11 @@ def MTX_Convert_10x_MEX_to_10x_HDF5_Format(
     # write data
     arr = df_mtx.read_count.values
     flag_dtype_is_integer = np.issubdtype(arr.dtype, np.integer)  # check integer dtype
+    ''' round the values in the matrix to convert float to integer '''
+    if flag_round_float and not flag_dtype_is_integer : # if dtype is float, and 'flag_round_float' flag is set to True, round the values
+        arr = np.rint( arr ).astype( int ) # convert to integer type (round to the nearest integer)
+        arr[ arr == 0 ] = 1 # convert entries with 0 values counts to 1, so that the minimum value in the matrix is 1
+        flag_dtype_is_integer = True # update the flag 
     mtx.create_dataset("data", (len(arr),), "i8" if flag_dtype_is_integer else "f", arr)
 
     # write indices
